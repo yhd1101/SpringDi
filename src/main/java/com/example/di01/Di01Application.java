0c443c25505baf82ae3f31a00d98ee5bbf003018
양한동 <yhd0121@gs.anyang.ac.kr>
2024-11-14 14:05:34 +0900

    first commit

---
+++ src/main/java/com/example/di01/Di01Application.java
@@ -0,0 +1,13 @@
+package com.example.di01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Di01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Di01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/di01/Di01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.di01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Di01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
